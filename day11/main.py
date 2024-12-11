8a7adc1c9cdf405fcaada2881e3bce6ae8c9fbb8
Rahul Yedida <yrahul3910@gmail.com>
2024-12-11 01:35:46 -0500

    Day 11: Python implementation

--- day11/main.py
+++ day11/main.py
@@ -1,53 +1,44 @@
-dp = {0: ([1], 1)}
+dp = {0: [(1, 1)]}
 
 def evolve(arr):
+    ctr = {}
+    for val, ct in arr:
+        ctr[val] = ctr.get(val, 0) + ct
+
     n = len(arr)
     if n == 0:
-        return ([], 0)
+        return []
     if n == 1:
-        if arr[0] in dp:
-            return dp[arr[0]]
-        if len(f"{arr[0]}") % 2 == 0:
-            s = str(arr[0])
+        value = arr[0][0]
+        if value in dp:
+            return dp[value]
+        if len((s := str(value))) % 2 == 0:
             m = len(s)
-            dp[arr[0]] = ([int(s[:m//2]), int(s[m//2:])], 2)
-            return dp[arr[0]]
+            dp[value] = [(int(s[:m//2]), 1), (int(s[m//2:]), 1)]
+            return dp[value]
         else:
-            v = arr[0] * 2024
-            dp[arr[0]] = ([v], 1)
+            v = value * 2024
+            dp[value] = [(v, 1)]
+            return dp[value]
 
-    p1 = evolve(arr[:n//2])
-    p2 = evolve(arr[n//2:])
-    return [*(p1[0]), *(p2[0])], p1[1] + p2[1]
+    items = {}
+    for v, c in ctr.items():
+        res = evolve([(v, c)])
+        for el, _c in res:
+            items[el] = items.get(el, 0) + _c * c
+
+        if v not in dp:
+            dp[v] = res
+
+    return list(items.items())
 
 def p1(arr):
-    a = arr[:]
+    a = [(x, 1) for x in arr]
     for _ in range(75):
         print(f"it {_}")
-        a, s = evolve(a)
-    print(s)
-
-p1([125, 17])
+        a = evolve(a)
+    print(sum([x[1] for x in a]))
 
 arr = [int(x) for x in open("input.txt", "r").read().split(" ")]
 p1(arr)
 
-# buf = ["x" for _ in range(65535)]
-# buf = open('tiny.txt', 'r').read().split(" ")
-#
-# for _ in range(25):
-#     print(f"{_ = }")
-#     for i, v in enumerate(buf):
-#         if v == "x":
-#             break
-#
-#         val = int(v)
-#
-#         if val == 0:
-#             buf[i] = "1"
-#         elif len(v) & 1:
-#             buf[i] = f"{2024*val}"
-#         else:
-#             buf = buf[:i] + [v[:len(v)//2], v[len(v)//2:]] + buf[i+1:]
-#
-# print(len(buf))
