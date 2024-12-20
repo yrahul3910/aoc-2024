9baed9f440c5fefa49ab7c6853d89ae50d6bf56f
Rahul Yedida <yrahul3910@gmail.com>
2024-12-20 00:55:19 -0500

    Add day 20 Python code

---
+++ day20/main.py
@@ -0,0 +1,41 @@
+from collections import deque
+import itertools
+
+grid = open('input.txt').read().splitlines()
+m = len(grid)
+n = len(grid[0])
+
+start_row = [i for i, row in enumerate(grid) if 'S' in row][0]
+start_col = grid[start_row].index('S')
+
+def bfs():
+    visited = [[0 for _ in range(n)] for _ in range(m)]
+    visited[start_row][start_col] = 1
+
+    q = deque([])
+    q.append((start_row, start_col, 0))
+    dist = {(start_row, start_col): 0}
+
+    while q:
+        x, y, d = q.popleft()
+
+        if grid[x][y] == 'E':
+            break
+
+        for dx, dy in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+            nx, ny = x + dx, y + dy
+
+            if 0 <= nx < m and 0 <= ny < n and visited[nx][ny] == 0 and grid[nx][ny] != '#':
+                q.append((nx, ny, d + 1))
+                dist[(nx, ny)] = d + 1
+                visited[nx][ny] = 1
+
+    count = 0
+    for (x1, y1), (x2, y2) in itertools.combinations(dist.keys(), 2):
+        if 1 < abs(x1 - x2) + abs(y1 - y2) <= 2:  # for part 2, change this to 20
+                if abs(dist[(x1, y1)] - dist[(x2, y2)]) >= 100 + abs(x1 - x2) + abs(y1 - y2):
+                    count += 1
+
+    return count
+
+print(bfs())
