22f5f177eb3d54aed72c329b5b3f9292edf256c3
Rahul Yedida <yrahul3910@gmail.com>
2024-12-25 00:44:52 -0500

    Code improvements (but still fails on real input)

--- day24/main.c
+++ day24/main.c
@@ -1,21 +1,30 @@
-#include <stdio.h>
 #include <stdbool.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #define MAX_SIZE 768
-
-int visited[MAX_SIZE];
-char unhash[MAX_SIZE][4];
+#define VAR_LENGTH 4
+#define BUFSIZE 20
 
 struct Equation {
-    char lhs[4];
+    char lhs[VAR_LENGTH];
     int opcode;
-    char rhs[4];
-    char result[4];
+    char rhs[VAR_LENGTH];
+    char result[VAR_LENGTH];
 };
 
-// djb2 hash
+// Global to avoid stack overflow with large arrays
+// and maintain consistency across recursive calls
+int visited[MAX_SIZE];
+char unhash[MAX_SIZE][VAR_LENGTH];
+
+/**
+ * Computes a hash value for a string using the djb2 algorithm.
+ * Collision handling: Currently exits on collision.
+ * @param str Input string
+ * @return Hash value modulo MAX_SIZE
+ */
 unsigned long hash(const char *str) {
     unsigned long hash = 5381;
     int c;
@@ -46,9 +55,15 @@ int xor (int a, int b) { return a ^ b; }
     return -1;
 }
 
-void visit(int node, int adj[MAX_SIZE][MAX_SIZE], int *visited, int *list,
-           size_t *idx) {
-
+/**
+ * Visit a node in a DFS fashion.
+ * @param node The node to visit
+ * @param adj Adjacency matrix
+ * @param visited Visited nodes tracking
+ * @param unhash Hash to node name mapping
+ * @param list Output sorted list
+ */
+void visit(int node, int adj[MAX_SIZE][MAX_SIZE], int *list, size_t *idx) {
     if (strlen(unhash[node]) < 2)
         return;
 
@@ -61,32 +76,66 @@ void visit(int node, int adj[MAX_SIZE][MAX_SIZE], int *visited, int *list,
     // For simplicity, ignore cycles
     for (size_t i = 0; i < MAX_SIZE; i++) {
         if (adj[i][node]) {
-            visit(i, adj, visited, list, idx);
+            visit(i, adj, list, idx);
         }
     }
 
     list[(*idx)++] = node;
 }
 
-size_t toposort(char nodes[MAX_SIZE][4], size_t nodecount,
+/**
+ * Performs topological sort on the graph.
+ * @param nodes Array of node names
+ * @param nodecount Number of nodes
+ * @param adj Adjacency matrix
+ * @param list Output sorted list
+ * @return Number of nodes in sorted order
+ */
+size_t toposort(char nodes[MAX_SIZE][VAR_LENGTH], size_t nodecount,
                 int adj[MAX_SIZE][MAX_SIZE], int *list) {
     size_t idx = 0;
 
     for (size_t i = 0; i < nodecount; i++) {
         size_t cur_hash = hash(nodes[i]);
-        visit(cur_hash, adj, visited, list, &idx);
+        visit(cur_hash, adj, list, &idx);
     }
 
     return idx;
 }
 
-int main() {
-    FILE *fp = fopen("input.txt", "r");
+/**
+ * Cleanup allocated resources and exit.
+ * @param equations Array of equation pointers to free
+ * @param eq_size Number of equations
+ * @param fp File pointer to close
+ */
+void cleanup(struct Equation **equations, size_t eq_size, FILE *fp) {
+    for (size_t i = 0; i < eq_size; i++) {
+        free(equations[i]);
+    }
 
-    int adj[MAX_SIZE][MAX_SIZE];
+    free(equations);
+    fclose(fp);
+    exit(1);
+}
+
+int main() {
+    FILE *fp = fopen("medium.txt", "r");
+
+    if (!fp) {
+        fprintf(stderr, "Could not open file.\n");
+        exit(1);
+    }
+
+    int adj[MAX_SIZE][MAX_SIZE] = {0};
     int values[MAX_SIZE];
-    struct Equation **equations =
-        (struct Equation **)malloc(MAX_SIZE * sizeof(struct Equation *));
+    struct Equation **equations = malloc(MAX_SIZE * sizeof(struct Equation *));
+
+    if (equations == NULL) {
+        fprintf(stderr, "Could not allocate memory.\n");
+        exit(1);
+    }
+
     int eq_hash[MAX_SIZE];
     int eq_idx = 0;
 
@@ -98,53 +147,60 @@ int main() {
         values[i] = -1;
     }
 
-    char buf[20];
+    char buf[BUFSIZE];
     char *cur;
-    while (fgets(buf, 20, fp) != NULL && strlen(buf) > 1) {
+    while (fgets(buf, BUFSIZE, fp) != NULL && strlen(buf) > 1) {
         cur = strtok(buf, " :\n");
         char *val = strtok(NULL, " :\n");
 
         size_t cur_hash = hash(cur);
-        strcpy(unhash[cur_hash], cur);
+        snprintf(unhash[cur_hash], VAR_LENGTH, "%s", cur);
 
         if (values[cur_hash] != -1) {
             fprintf(stderr, "Warning: collision detected: got %d\n",
                     values[cur_hash]);
-            exit(1);
+            cleanup(equations, eq_idx, fp);
         }
         values[cur_hash] = atoi(val);
     }
 
     // Important: run truncate -s -1 tiny.txt if the input has a newline at the
     // end.
-    char nodes[MAX_SIZE][4];
+    char nodes[MAX_SIZE][VAR_LENGTH];
     int idx = 0;
 
-    while (!feof(fp)) {
-        fgets(buf, 20, fp);
+    while (fgets(buf, BUFSIZE, fp) != NULL) {
         char *lhs = strtok(buf, " ->\n");
         char *op = strtok(NULL, " ->\n");
         char *rhs = strtok(NULL, " ->\n");
         char *val = strtok(NULL, " ->\n");
 
-        printf("%s %s %s\n", lhs, rhs, val);
+        if (!lhs || !op || !rhs || !val) {
+            fprintf(stderr, "Warning: parsing %s had errors.\n", buf);
+            cleanup(equations, eq_idx, fp);
+        }
 
-        strcpy(unhash[hash(lhs)], lhs);
-        strcpy(unhash[hash(rhs)], rhs);
-        strcpy(unhash[hash(val)], val);
+        snprintf(unhash[hash(lhs)], VAR_LENGTH, "%s", lhs);
+        snprintf(unhash[hash(rhs)], VAR_LENGTH, "%s", rhs);
+        snprintf(unhash[hash(val)], VAR_LENGTH, "%s", val);
 
         adj[hash(lhs)][hash(val)] = opcode(op);
         adj[hash(rhs)][hash(val)] = opcode(op);
 
         equations[eq_idx] = (struct Equation *)malloc(sizeof(struct Equation));
-        strcpy(equations[eq_idx]->lhs, lhs);
-        strcpy(equations[eq_idx]->rhs, rhs);
-        strcpy(equations[eq_idx]->result, val);
+        if (!equations[eq_idx]) {
+            fprintf(stderr, "Memory allocation failed\n");
+            cleanup(equations, eq_idx, fp);
+        }
+
+        snprintf(equations[eq_idx]->lhs, VAR_LENGTH, "%s", lhs);
+        snprintf(equations[eq_idx]->rhs, VAR_LENGTH, "%s", rhs);
+        snprintf(equations[eq_idx]->result, VAR_LENGTH, "%s", val);
         equations[eq_idx]->opcode = opcode(op);
         eq_hash[hash(val)] = eq_idx;
         eq_idx++;
 
-        strcpy(nodes[idx++], val);
+        snprintf(nodes[idx++], VAR_LENGTH, "%s", val);
     }
 
     int order[MAX_SIZE];
@@ -201,4 +257,6 @@ int main() {
             }
         }
     }
+
+    cleanup(equations, eq_idx, fp);
 }
